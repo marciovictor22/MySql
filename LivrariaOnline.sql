d8ee5b0eade688cd5ae90c7e2d6eee18a687f967
marciovictor22 <51381814+marciovictor22@users.noreply.github.com>
2020-01-01 21:35:22 -0300

    Add files via upload

---
+++ LivrariaOnline.sql
@@ -0,0 +1,19 @@
+create database if not exists livraria;
+use livraria;
+
+create table if not exists livro(
+nome varchar(20) not null unique,
+descricao text,
+total_paginas int,
+autor varchar(30) not null unique  primary key,
+estoqe int,
+quantidade int
+);
+
+create table if not exists cliente(
+id int not null auto_increment primary key,
+nome varchar (30),
+email varchar(40),
+telefone varchar(12),
+itens_venda int
+);
